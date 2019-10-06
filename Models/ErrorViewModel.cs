c3a1f84e32b0c538d3a7d5cb9dfe5fab426c65fa
f3llps <fellipeversiani@gmail.com>
2019-10-06 15:22:46 -0300

    Incluir projeto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace project_euller_solutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
